db7f937f1c6e95dedfe0ad5c08a2e8941aa468ec
Alejandro Marin <ingenieroalejandromarin@gmail.com>
2024-05-27 14:21:05 -0500

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KronosSolucionesTIC/godesde0
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/KronosSolucionesTIC/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
